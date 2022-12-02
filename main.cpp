d12bbecd560184fb886539a7fc33c5cbd3db5185
Farhan Tahmid <farhantahmid881@gmail.com>
2022-12-02 19:31:56 +0600

    initial

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    private:
+    int employeeID;
+    string firstName;
+    string lastName;
+    float workingHours;
+    float costPerHour;
+    float deduction;
+    float annualSalary;
+    public:
+    void set_employeeiD();
+
+};
+
+
+
+
+
+int main(){
+    cout<<"Welcome to the Human Resource Management Project!";
+}
