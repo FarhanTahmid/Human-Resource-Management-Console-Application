f9090d9fc5711cb54dbd44d6c69911327dcacfb0
Farhan Tahmid <farhantahmid881@gmail.com>
2022-12-04 00:50:13 +0600

    delete ready

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include<fstream>
 #include<stdlib.h>
 #include <math.h>
 #include <string>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 ////global variables for total number, array, string and id
 int initialID = 1001; //as id starts with 1001
-int n = 0; //To track total number of employees
 int i = 0; //to control loop while multiple employee input and control the array Employee emp[100]
 int y[100];
 int z[100];//to store salary, if not needed remove
@@ -21,9 +22,9 @@ class Employee
 {
 private:
     int employeeID;
-    string firstName; 
-    string lastName;  
-    
+    string firstName;
+    string lastName;
+
     double annualSalary;
     float workingHours;
     float costPerHour;
@@ -33,10 +34,12 @@ public:
     void set_fieldName();
     void get_fieldName();
     void get_deletingemployeeDetails();
+    void get_deletingConfirmation();
     void set_employeeID(int);
     int get_employeeID();
     void set_Name();
     void set_firstName();
+    void set_firstName(string);
     void set_lastName();
     void set_workingHour();
     void set_costPerHour();
@@ -53,19 +56,25 @@ void ::Employee::set_firstName()
 {
     cout << "Enter First Name of the Employee: ";
     while (!(cin >> this->firstName)){// check if character or not
-        
+
         cout << "Try again: ";
         cin.clear();
         cin.ignore(1000, '\n');
 
     }
 }
+
+void ::Employee::set_firstName(string fname)
+{
+    this->firstName=fname;
+}
+
 void ::Employee::set_lastName()
 {
     cout << "Enter Last Name of the Employee: ";
 
     while (!(cin >> this->lastName)){// check if character or not
-        
+
         cout << "Try again: ";
         cin.clear();
         cin.ignore(1000, '\n');
@@ -91,8 +100,8 @@ void ::Employee::set_workingHour()
         cout << "\nPlease enter the working hours between 1 and 60! Try again: \n";
         set_workingHour();
     }
-    
-    
+
+
 }
 void ::Employee::set_costPerHour()
 {
@@ -114,32 +123,33 @@ void ::Employee::set_costPerHour()
         cout << "\nPlease enter the Cost Per Hour a positive Number! Try again: ";
         set_costPerHour();
     }
-    
+
 }
 void ::Employee::set_deduction(){
 
     try{
         cout << "Set The amount you want to deduct from this employee's annual Salary: ";
-        float ded;
-        cin>>ded;
+        float dd;
+        cin>>dd;
         cin.clear();
         cin.ignore(1000, '\n');
-        if ((ded)< 0){
+        if ((dd)< 0){
             cout << "\nPlease enter deduction amount a positive number! Try again: ";
             set_deduction();
         }
         else{
-            this->deduction = ded;
+            this->deduction = dd;
+            //cout<<this->deduction;
         }
     }
     catch(invalid_argument){
         cout << "\nPlease enter deduction amount a positive number! Try again: ";
         set_deduction();
     }
-    
+
 }
 void ::Employee::set_annualSalary(){
-    this->annualSalary=((double(this->workingHours)*double(this->costPerHour))*52.143)-double(this->deduction);  
+    this->annualSalary=((this->workingHours*this->costPerHour)*52.0)-(this->deduction);
 }
 double ::Employee::get_annualSalary(){
     return this->annualSalary;
@@ -149,8 +159,12 @@ void ::Employee ::get_fieldName()
     cout << this->employeeID << "\t\t" << this->firstName << "\t\t\t" << this->lastName << "\t\t\t" << this->annualSalary << "\t" << endl;
 }
 void::Employee::get_deletingemployeeDetails(){
-    cout<<"Do you really want to delete the employee with ID: "<<this->employeeID<<" First Name:"<<this->firstName<<"\t"<<" Last Name:"<<this->lastName<< "(y/n)?:";
+    cout<<"Do you really want to delete employee " <<this->firstName<<" "<<this->lastName<< " (y/n)?:"<<endl;
 }
+void::Employee::get_deletingConfirmation(){
+    cout<<"The employee " <<this->firstName<<" "<<this->lastName<< " was deleted!"<<endl;
+}
+
 int::Employee::get_employeeID(){
     return this->employeeID;
 }
@@ -167,9 +181,7 @@ void ::Employee ::set_fieldName()
     set_workingHour();
     set_costPerHour();
     set_annualSalary();
-    z[i] = annualSalary;//remove if not needed further
-    salary[i] = annualSalary;
-    h[i] = lastName;//storing last name, remove if not needed
+    set_deduction();
     y[k] = employeeID;
 
     k++;
@@ -178,16 +190,20 @@ void ::Employee ::set_fieldName()
 
 class HRM
 {
-private:
-    Employee emp[100];
-    Employee temp[100];
+
+
+
 
 public:
     void AddEmployee();
-    void DeleteEmployee();
-    bool SearchEmployee();
-    void UpdateEmployee();
-    void ReportList();
+    void deleteEmployee();
+    int searchEmployee();
+    int searchEmployee(int);
+    void updateEmployee();
+    int number; //To track total number of employees
+    Employee emp[100];
+    Employee temp[100];
+
 };
 
 
@@ -198,197 +214,211 @@ void ::HRM::AddEmployee()
     char again;
     do
     {
-        n++; //Track of total Number of employees
+        number++; //Track of total Number of employees
         emp[i].set_fieldName(); //setting the employee on current point of i
         i++; //incrementing i
         cout<<i<<"\n";
 
         cout << "\nThe employee with the following information has been recorded:" << endl;
-        cout << "\nEmployee Id       First Name         Last Name           Annual Salary ($)";
+        cout << "\nEmployee Id       First Name         Last Name             Salary Per Year ($)";
         cout << "\n------------   -----------------  ---------------     -------------------------" << endl;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < number; i++)
         {
             emp[i].get_fieldName();
         }
-        cout << "Do u wont to add another employee?\nIf yes, press 'y' or press any other key to exit!" << endl;
+        cout << "Do you want to add another employee?(y/n)" << endl;
         cin >> again;
     } while ((again == 'y' || again == 'Y'));
 }
 
-bool HRM::SearchEmployee()
+//search Employee with ID.
+int HRM::searchEmployee()
 {
     int searchID;
     char searchAgain;
     bool found;
-    do
-    {   
-        cout<<"Enter Employee ID to search an Employee: ";
-        cin>>searchID;
-        for(int i=0;i<n;i++){
-            if(emp[i].get_employeeID()==searchID){
-                cout<<"Employee Found with this ID!"<<endl;
-                cout<<"Details:"<<endl;
-                cout << "\nEmployee ID       First Name       Last Name          Annual Salary ($)";
-                cout << "\n------------   -------------  --------------   -----------------------" << endl;
-                emp[i].get_fieldName();
-                found=true;
-                break;
-            }else{
-                found=false;
+    if(number!=0){
+        do
+        {
+            cout<<"Enter Employee ID to search an Employee: ";
+            cin>>searchID;
+            for(int i=0; i<number; i++)
+            {
+                if(emp[i].get_employeeID()==searchID)
+                {
+                    cout<<"Employee Found with this ID!"<<endl;
+                    cout<<"Details:"<<endl;
+                    cout << "\nEmployee ID       First Name     Family Name             Annual Salary ($)";
+                    cout << "\n------------   ----------------  -------------------   -----------------------" << endl;
+                    emp[i].get_fieldName();
+                    found=true;
+                    break;
+                }
+                else
+                {
+                    found=false;
+                }
             }
+            if(found==false)
+            {
+                cout<<"No Employee Was found with this ID!";
+            }
+            cout << "\nDo you want to Search again?(y/n)?"<< endl;
+            cin >> searchAgain;
         }
-        if(found==false){
-            cout<<"No Employee Was found with this ID!";
+        while (searchAgain == 'y' || searchAgain == 'Y');
+        return searchID;
+    }else{
+        "The Record is Empty!"
+    }
+
+}
+
+//search employee to delete
+int HRM::searchEmployee(int searchID){
+    int deleteIndex=-1;
+    bool found;
+    for(int i=0; i<number; i++)
+    {
+        if(emp[i].get_employeeID()==searchID)
+        {
+            found=true;
+            deleteIndex=i;
+            break;
         }
-        cout << "\nDo you want to Search again?(y/n)?"<< endl;
-        cin >> searchAgain;
-    } while (searchAgain == 'y' || searchAgain == 'Y');
-    return found;
+        else
+        {
+            found=false;
+        }
+    }
+    if(found==false)
+    {
+        cout<<"No Employee Was found with this ID!";
+        return -1;
+    }
+    return deleteIndex;
 }
 
 
 //Delete Employee
-void HRM::DeleteEmployee()
+void HRM::deleteEmployee()
 {
-    //      Show all of the current employees
-    //      Ask the user the ID of which employee that they wish to
-    //      delete.
+    if(number!=0){
+        int empIdtoDelete;
+        bool found;
+        char redo;
+        char deleteChoice;
+        found = false;
+    label1:
+        do{
+            cout << "Please Enter the Employee ID: ";
 
-    int empIdtoDelete;
-    bool found;
-    char redo1;
-    char deleteChoice;
-    found = false;
-label1:
-    cout << "ID of employee to remove: ";
-
-    while (!(cin >> empIdtoDelete))//check if string or not
-    {
-        cout << "Please enter a number! Try again: ";
-        cin.clear();
-        cin.ignore(1000, '\n');
-    }
-    for (i = 0; i < n; ++i)
-    {
-
-        if (y[i] == empIdtoDelete)
-        {
-            emp[i] = emp[i + 1];
-            found = true;
-            emp[i].get_deletingemployeeDetails();
-            cin >> deleteChoice;
-            if (deleteChoice == 'y' || deleteChoice == 'Y')
+            while (!(cin >> empIdtoDelete))//check if string or not
             {
-                int swap;
-                swap = n;
-                cout << "\nThe employee with the following information has been added to the system:" << endl;
-                cout << "\nEmployee ID       First Name       Last Name          Annual Salary ($)";
-                cout << "\n--------------   --------------  ----------------   -------------------------" << endl;
-                for (int i = 0; i < swap; i++)
-                {
-                    emp[i].get_fieldName();
-                    cout << "hahahahah=" << n << endl;
-                    swap--;
-                    n = swap;
-                    n++;
-                }
-            }else{
-                goto label1;
+                cout << "Please enter a number! Try again: ";
+                cin.clear();
+                cin.ignore(1000, '\n');
             }
+            int getIndex=searchEmployee(empIdtoDelete);
 
-            cout << endl;
+            if(getIndex!=-1)
+            {
+                found=true;
+                emp[getIndex].get_deletingemployeeDetails();
+                cin>>deleteChoice;
+                if (deleteChoice == 'y' || deleteChoice == 'Y'){
+                    for(int i=0; i<number; i++)
+                    {
+                        if(empIdtoDelete==emp[i].get_employeeID())
+                        {
+                            emp[i].get_deletingConfirmation();
+                            emp[i]=emp[i+1];
+                            number--;
+                        }
+                    }
+                }
 
-            // Delete function
-        }
-    }
+            }
+            cout<<"Do you want to repeat delete by Entering new Employee ID? (y/n)"<<endl;
+            cin>>redo;
+        }while (redo == 'y' || redo == 'Y');
 
-    if (found == false)
-    {
-        cout << "Sorry, there is not any employee with requested Employee ID.\nDo you want to try again? (y/n)?:";
-        cin >> redo1;
-        if (redo1 == 'Y' || redo1 == 'y')
+        if (found == false)
         {
-            goto label1;
-            cout << endl
-                 << endl;
+            cout << "Sorry, there is no employee with requested Employee ID.\nDo you want to try again? (y/n)?:";
+            cin >> redo;
+            if (redo == 'Y' || redo == 'y')
+            {
+                goto label1;
+                cout << endl;
+            }
         }
+    }else{
+        cout<<"Your record is empty!";
     }
-
-    //      Delete the chosen employee from the array of employees
-    //      as represented in this class.
-
-    // Actually remove the entry from memory so as to not leak objects
-    // nextIndex--;
 }
+
+
+
 
 
 int main()
 {
-    //-------defining variables and initializing them-------------
 
-    // HRM info;
+    // readFromFile();
 
-    // int c;
-    // char operation, ch;
-    // //--------Printing my name on screen----------------
-    // cout << "Welcome to the  program 2.1 written by Your Name" << endl;
-    // cout << "**************************************************************************" << endl;
-    // cout << endl
-    //      << endl
-    //      << endl;
-    // do
-    // {
-
-    //     cout << "Welcome to Human Resource Management (HRM) Software of Company XYZ.";
-    //     cout << "To do specific task please choose one of the following commands." << endl
-    //          << endl
-    //          << endl;
-    //     cout << "    1. Add new employee" << endl;
-    //     cout << "    2. Delete employee information" << endl;
-    //     cout << "    3. Update employee information" << endl;
-    //     cout << "    4. Make reports based on specific field" << endl;
-    //     cout << "    5. Search employee" << endl;
-    //     cout << "    6. Quit" << endl
-    //          << endl;
-
-    //     while (!(cin >> c)) // Reciving vaiables from input : is it no/character ?
-    //     {
-    //         cout << "Please  enter a number!  Try again: ";
-    //         cin.clear();
-    //         cin.ignore(1000, '\n'); // Skip to next newline or 1000 chars,
-    //                                 // whichever comes first.
-    //     }
-    //     switch (c)
-    //     {
-    //     case 1:
-    //         cout << "\nEnter the information of the new employee" << endl;
-    //         info.AddEmployee();
-    //         break;
-    //     case 2:
-    //         info.DeleteEmployee();
-    //         break;
-    //     case 3:
-    //         cout << "\nEnter an item to deletion";
-    //         info.UpdateEmployee();
-    //         break;
-    //     case 4:
-    //         cout << "\nEnter an element to search";
-    //         info.ReportList();
-    //         break;
-    //     case 5:
-    //         info.SearchEmployee();
-    //         break;
-    //     default:
-    //         cout << "\nInvalid option try again";
-    //     }
-    //     cout << "\ndo u want to continue";
-    //     cin >> ch;
-    // } while (ch == 'y' || ch == 'Y');
-
-    // system("pause");
+    int menuChoice;
+    char again;
+    cout << "**************************************************************************" << endl;
+    cout << "                   HUMAN RESOURCE MANAGEMENT PROGRAM" << endl;
+    cout << "**************************************************************************" << endl;
+    cout << endl<< endl;
     HRM employee;
-    employee.AddEmployee();
-    cout<<employee.SearchEmployee();
+    do
+    {
+
+        cout << "Welcome...";
+        cout << "Please type the number of the options you want to choose :" << endl;
+        cout << "    1. Add new Employee" << endl;
+        cout << "    2. Delete an Employee" << endl;
+        cout << "    3. Update Employee Information" << endl;
+        cout << "    4. Search for an Employee" << endl;
+        cout << "    5. Quit" << endl;
+        cout<<"Please Enter your choice 1..5: ";
+        cout<< endl;
+
+        while (!(cin >> menuChoice)){
+            cout << "Please Enter a number between 1-5! Try again: ";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+        switch (menuChoice)
+        {
+        case 1:
+            cout<<"## Add an Employee ##"<<endl;
+            employee.AddEmployee();
+            break;
+        case 2:
+            cout<<"## Delete an Employe ##"<<endl;
+            employee.deleteEmployee();
+            break;
+        case 3:
+            employee.updateEmployee();
+            break;
+        case 4:
+            cout<<"## Update an Employee ##"<<endl;
+            employee.searchEmployee();
+            break;
+        case 5:
+            system("pause");
+            break;
+        default:
+            cout << "\nInvalid option try again";
+        }
+        cout << "\nDo u want to perform any other task?(y/n)";
+        cin >> again;
+    } while (again == 'y' || again == 'Y');
+
     return 0;
 }
